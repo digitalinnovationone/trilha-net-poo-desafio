4d785142d17bd0cdf69068bf95e6b4e4e57a7534
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-05-22 22:01:58 -0300

    Inicial

---
+++ Models/Iphone.cs
@@ -0,0 +1,8 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone
+    {
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+}
---
+++ Models/Smartphone.cs
@@ -0,0 +1,26 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
