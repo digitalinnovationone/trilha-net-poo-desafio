f84ab8730b85308fc18ac73d8358f08fcc5c0c15
DouglasSoares1203 <douglashardware13@gmail.com>
2022-09-25 21:54:52 -0300

    commit inicial

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero) : base(numero)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
