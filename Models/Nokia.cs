2547bf5fbe05c36992b3af4c0f979d12ecfc59e5
neg0uiu <willianx2002.ws@gmail.com>
2023-06-03 16:36:51 -0300

    Adicionado propriedades solicitadas e implementada

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
