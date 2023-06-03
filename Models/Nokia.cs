3e77d910cfd0d147275ab8e8f75e60fe6a3d6fa0
neg0uiu <willianx2002.ws@gmail.com>
2023-06-03 17:07:19 -0300

    Implementado metodo construtor nas classes filhas

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,9 +4,13 @@ namespace DesafioPOO.Models
     public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia (string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+            
+        }
         public override void InstalarAplicativo(string nomeApp)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Aplicativo {nomeApp} foi instalado com sucesso no Nokia!");
         }
     }
 }
