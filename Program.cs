6d9aa2817b98c95fd30b093ac4ed5ca93889e9b3
Rian Negreiros Dos Santos <72347639+RianNegreiros@users.noreply.github.com>
2023-09-23 13:43:08 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Criar objetos Nokia e Iphone
+        Nokia nokia = new Nokia("123456789");
+        Iphone iphone = new Iphone("987654321");
+
+        // Realizar ações nos smartphones
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+
+        // Pausa para que você possa ver a saída antes de fechar o programa
+        Console.ReadLine();
+    }
+}
