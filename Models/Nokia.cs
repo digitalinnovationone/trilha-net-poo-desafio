febab686558ee8385c1947e8b92012a1ffd358f8
Rian Negreiros Dos Santos <72347639+RianNegreiros@users.noreply.github.com>
2023-09-23 13:42:21 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero) : base(numero)
+        {
+            // Inicializar propriedades específicas de Nokia, se necessário
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na Nokia...");
+            // Implementação específica para a instalação de aplicativos em um Nokia
+        }
     }
 }
