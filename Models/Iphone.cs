36f5576e4ae72f261bf72da06ec73dabe6dee3d5
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-10-06 14:34:02 -0300

    feat: add constructor in Iphone class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,6 +3,10 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
