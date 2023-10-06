7373fe60814de60101ea5e107ef309f4a40721a0
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-10-06 14:33:31 -0300

    feat: add class inheritance and metod everride in Iphone class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Iphone");
+        }
     }
 }
