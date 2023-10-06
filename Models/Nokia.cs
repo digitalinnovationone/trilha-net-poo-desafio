29d7e25c1acf6cdf995854d01df012089af777fa
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-10-06 14:39:10 -0300

    docs: remove some comments

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,13 +1,12 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
 
         }
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando aplicativo '{nomeApp}' no Nokia");
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,6 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
         protected string Modelo;
         protected string IMEI;
         protected int Memoria;
@@ -11,7 +10,6 @@ namespace DesafioPOO.Models
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
