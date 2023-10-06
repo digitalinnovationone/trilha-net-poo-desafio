8824e4afb7efd6170425f08a511cada2f7c4cf5e
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-10-06 14:24:12 -0300

    feat: add working app model

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "12341234", modelo: "Modelo Nokia", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone(numero: "43214321", modelo: "Iphone 12", imei: "222222222", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
