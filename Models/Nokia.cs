ab2d94cf73bb89c34fdfd277f6aa6cd0439e95b4
Gláucia Cecília Pivoto Andare <glaucia.andare@gmail.com>
2023-10-13 14:05:50 -0300

    Finalizando o projeto

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero,modelo,imei,memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nome} em um Nokia {Modelo}.");
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("123456789", "Nokia 3310", "1234", 16);
+        iPhone iphone = new iPhone("987654321", "iPhone 12 Pro", "5678", 64);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
