72d4f1942e5aefe9195f80d42b77ea0acf734663
guifx <guifxb@yahoo.com>
2023-11-04 15:42:47 -0300

    solved challenge

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,9 +5,17 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "98888888", modelo: "Tijolo", imei: "999999", memoria: 10);
+nokia.Ligar();
+nokia.InstalarAplicativo("X");
+
+Console.WriteLine('\n');
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "22222228", modelo: "Estevão Trabalhos", imei: "000000", memoria: 30);
+iphone.Ligar();
+iphone.InstalarAplicativo("Twitter");
