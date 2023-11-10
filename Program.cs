b227150f440ecf5f4db71fb3ba1b1469239c0739
everton-paulino <evertonpaulino25@gmail.com>
2023-11-09 21:22:17 -0300

    Initial Comit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo1", imei: "1111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo2", imei: "2222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
