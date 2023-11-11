00d897f179c70ad413a537cb37d25f31eee88302
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-11-11 01:20:02 -0300

    :hammer:add ascript de desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456009", "ModeloNokia", "IMEINokia", 32);
+
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Iphone iphone = new Iphone("987677321", "ModeloIphone", "IMEIIphone", 64);
+
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
