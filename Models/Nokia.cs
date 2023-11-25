e248e6c82be7088bc571e524c0d691804df037f9
Nathan <nathanhs002@gmail.com>
2023-11-25 15:52:49 -0300

    Classe Nokia, herdeira de Smartphone foi implementada

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,21 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor da classe Nokia
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Adicionar lógica específica do construtor da classe Nokia, se necessário
+        }
+
+        // Sobrescrever o método InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Nokia...");
+            // TODO: Adicionar a lógica de instalação específica para a Nokia
+        }
     }
 }
