2a07226423d13e806727ffbf83a9833e25e23a8e
Nathan <nathanhs002@gmail.com>
2023-11-25 15:53:41 -0300

    Testes implementados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Testando a classe Nokia
+        Nokia nokia = new Nokia("123456789", "Nokia ModeloX", "IMEI123", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
