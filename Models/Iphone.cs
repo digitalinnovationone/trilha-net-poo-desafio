746c432a70e721c5c9070e77051fce9f20ce79d6
Nathan <nathanhs002@gmail.com>
2023-11-25 15:50:35 -0300

    Classe Iphone, herdeira de Smartphone foi implementada

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,21 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor da classe Iphone
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Adicionar lógica específica do construtor da classe Iphone, se necessário
+        }
+
+        // Sobrescrever o método InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+            // TODO: Adicionar a lógica de instalação específica para o iPhone
+        }
     }
 }
