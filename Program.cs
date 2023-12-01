66ff367cdaddd3a83ae087b97de83d01ba10d948
Rosendo <ewerton.rosendo15@gmail.com>
2023-12-01 15:53:39 -0300

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program{
+    static void Main(){
+            Console.WriteLine("criando iphone");
+            Iphone iphone = new Iphone("432423", "iphone 13", "3423423", 64);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("DIO");
+
+            Console.WriteLine("criando NOKIA");
+            Nokia nokia = new Nokia("2374322", "nokia avanade", "232556", 128);
+
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("AVANADE");
+
+ 
+    }
+}
