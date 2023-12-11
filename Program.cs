0631125ee0aef92157b95806b9789c3602709b3b
igusouz <igorrafael.jobs@gmail.com>
2023-12-11 18:51:04 -0300

    Desafio finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program{
+    static void Main(){
+
+            //teste Nokia
+            Nokia nokiaTijolao = new Nokia("85859467", "Nokia Tijolão", "#6485", 32);
+            nokiaTijolao.Ligar();
+            nokiaTijolao.ReceberLigacao();
+            nokiaTijolao.InstalarAplicativo("Flappy Bird");
+
+            Console.WriteLine("----------------------------------------------");
+
+            //teste iphone
+            Iphone iphoneCapenga = new Iphone("01060309", "Iphone 6", "#0101", 16);
+            iphoneCapenga.Ligar();
+            iphoneCapenga.ReceberLigacao();
+            iphoneCapenga.InstalarAplicativo("YouTube");
+
+    }
+}
