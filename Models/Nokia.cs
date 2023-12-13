d2d9f4886ae3a0939c3b18df401326863552d4b2
Camila Xavier <camila.gabriela.xavier.santos@gmail.com>
2023-12-13 03:51:17 -0300

    Desafio Classes Abstratas Concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : 
+        base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} - Acesse a Play Store para baixar mais aplicativos do seu Nokia");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    // TODO: Realizar os testes com as classes Nokia e Iphone
+    static void Main() 
+    {
+        // Exemplo de uso
+        Nokia nokiaPhone = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "IMEI123", memoria: 128);
+        nokiaPhone.Ligar();
+        nokiaPhone.ReceberLigacao();
+        nokiaPhone.InstalarAplicativo("WhatsApp");
+
+        Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+
+}
+
