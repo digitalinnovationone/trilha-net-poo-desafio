6da4cd5c2221a2d8da634354140ab6ac72df29ba
Fylipe Araújo <fylipebabc@gmail.com>
2023-12-26 14:55:40 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,16 +1,25 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        // Propriedades da classe Smartphone
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // Construtor da classe Smartphone
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
+        // Métodos da classe Smartphone
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
@@ -21,6 +30,7 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato para instalar aplicativo
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
