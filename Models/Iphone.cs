32726bc5633bd70760d35e3b56df4b7ad8c9be3f
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-12-26 13:23:01 -0300

    Conclusão do desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string imei, string modelo, int memoria) : base(numero, imei, modelo, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+             Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
     }
 }
