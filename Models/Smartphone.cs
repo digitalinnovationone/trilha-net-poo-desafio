67d275f0a1847937f8f91f17301ac75f1e97136b
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-12-29 17:42:09 -0300

    Completando codigo de smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
