0e6393a7babd45c42def49bc3c0f417f3faca3be
Adriany Corrêa <adrianycmc@gmail.com>
2024-01-02 19:37:00 -0300

    Ajustes na implementação

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,6 +3,8 @@ using System.Runtime.CompilerServices;
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
+    // Implementado 
+    
     public class Iphone : Smartphone
     {
         public Iphone(string numero, string modelo, string imei, int memoria) : base (numero,  modelo, imei, memoria)
@@ -10,6 +12,7 @@ namespace DesafioPOO.Models
        
         }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Implementado
 
         public override void InstalarAplicativo(string nomeApp)
         {
