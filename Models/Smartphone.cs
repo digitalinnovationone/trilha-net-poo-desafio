3cab6ac468c452e232bceafc8b76270c163fd125
Marina Cavalcante <cavalcantemarina@gmail.com>
2024-01-07 12:45:09 -0300

    Desafio concluído!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,28 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria {get; set; }
+        
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+           
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando {Modelo} ...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação em {Modelo} ...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+         Console.WriteLine("\n");
+        Console.WriteLine("Smartphone modelo: Nokia");
+        Smartphone nokia = new Nokia("81 98161-0209","Nokia Tijolão", "000000", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+        Console.WriteLine("*************************************************************************************************************");
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone modelo: Iphone");
+        Smartphone iphone = new Iphone("81 99248-0209", "Iphone 50", "111111", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+        Console.WriteLine("\n");
+    }
+}
