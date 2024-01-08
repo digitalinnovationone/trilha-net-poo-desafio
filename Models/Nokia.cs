0a43ddb58dae5822520a086089850066963db1a5
Daniel Ventura <danidevbrgmail.com>
2024-01-08 16:02:11 -0300

    Desafio Realizado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia (string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+          {
+
+          }
+          public override void InstalarAplicativo(string nomeApp)
+          {
+            Console.WriteLine($"Instalando {nomeApp} no celular Numero {Numero}\n{Modelo}, {IMEI} e sua capacidade é de {Memoria} GB Mem.");
+          }
+            // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,32 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando do {Modelo}...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação no {Modelo}...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
