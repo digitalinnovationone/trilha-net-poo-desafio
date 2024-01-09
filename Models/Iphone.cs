57942441445386b89a52dd25ac1023e7764d328f
Hirla de Andrade <hhirlaandrade@gmail.com>
2024-01-09 18:52:31 -0300

    Desafio Concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria)  
+        {
+            
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){ 
+            
+            Console.WriteLine($"Instalando Aplicativo {nomeApp} no Iphone...");
+            Console.WriteLine($"{nomeApp} instalado com sucesso!");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
+    public class Nokia : Smartphone
+    { 
+        public Nokia(string numero, string modelo, string imei, int memoria)  
+        {
+            
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){ 
+            
+            Console.WriteLine($"Instalando Aplicativo {nomeApp} no Nokia...");
+            Console.WriteLine($"{nomeApp} instalado com sucesso!");
+        }
     }
 }
