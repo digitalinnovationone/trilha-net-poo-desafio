82d5cb0ec207f4e80b91ec0b81be4503e6c55ca7
Wictor Duarte <wictorduarte@outlook.com>
2024-01-13 20:22:34 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -15,12 +21,10 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Ligando...");
         }
-
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
