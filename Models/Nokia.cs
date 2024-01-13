5fe1793ef47e9dddc1565ff011e7711b54c9971e
ArlessonMoura <74433643+ArlessonMoura@users.noreply.github.com>
2024-01-12 23:46:37 -0300

    Full project

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,11 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp) {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} da Windows Store em seu {modelo}");
+        };
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Recebendo dados de seu smartphone, aguarde...");
+Smartphone iosMobile = new Iphone(numero: "998909050", modelo: "Iphone SE 1° Gen", imei: "i345d593un93ni12co" , memoria: 128);
+Iphone.Ligar();
+Iphone.InstalarAplicativo("Whatsapp");
+
+console.WriteLine("-------------------------------------------------------- \n")
+
+Console.WriteLine($"Recebendo dados de seu smartphone, aguarde...");
+Smartphone msLumia = new Nokia(numero: "998909050", modelo: "Nokia Lumia 710", imei: "i345d593un93ni12co" , memoria: 64);
+Iphone.Ligar();
+Iphone.InstalarAplicativo("Z0mb13");
