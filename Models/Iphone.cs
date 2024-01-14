49749d00d0dfde85f467a762eb6258d49b041330
Wictor Duarte <wictorduarte@outlook.com>
2024-01-13 21:39:09 -0300

    ec207f4e80b91ec0b81be4503e6c55ca7.
    modified:   Models/Nokia.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,13 +1,8 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone : Smartphone
+    public class Iphone
     {
-        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) {}
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nomeApp)
-        {
-            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Iphone.");
-        }
     }
 }
