241c55f5c512b5a58447fcad5aa44952dbc38548
geraldocarvalho <geraldo_a_carvalho@hotmail.com>
2024-01-13 22:11:41 -0300

    Desafio Concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,14 +1,8 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia : Smartphone
+    public class Nokia
     {
-        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) {}
-
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nomeApp)
-        {
-            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia.");
-        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,11 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        protected string Modelo;
-        protected string IMEI;
-        protected int Memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero, string modelo, string imei, int memoria)
+        public Smartphone(string numero)
         {
             Numero = numero;
-            Modelo = modelo;
-            IMEI = imei;
-            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -21,10 +15,12 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Ligando...");
         }
+
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
+
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
