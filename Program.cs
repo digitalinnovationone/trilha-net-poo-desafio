46a36e3f6b36338d085c9125946bb6486fd96632
Allan Lauzid <allanlauzid@gmail.com>
2024-01-29 04:35:31 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿using DesafioPOO.Models;
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
