4645728cddc0bc6df7bb548405c0ffdca58732eb
Gustavo Henrique Carvalho Campos <53590418+gustavohccampos@users.noreply.github.com>
2024-03-25 21:08:33 -0300

    Upload DesafioDIO_NET_POO_Celular

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+             : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        //Sobrescrevendo o método abastrato
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone!");
+        }
     }
 }
